4275fc082647961997311aaefd28b739e9286236
rocketxujia <rocketxj@gmail.com>
2021-05-27 14:27:48 +0800

    feat: queue-stack

---
+++ 20210425-queue-stack/1-stack.js
@@ -0,0 +1,18 @@
+/**
+ * 题目：使用队列queue来实现堆栈stack
+ */
+
+class Stack {
+    constructor() {}
+    push( elem ) {
+        // 使用产生一个新队列 newQueue 存放链接，由新元载elem作为对头，原队列所有元素按头尾顺序链接上去的。
+    }
+
+    /**
+     * 以下方法与queue一致，直接
+     */
+    pop() {}
+    empty() {}
+    size() {}
+}
+
